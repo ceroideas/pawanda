91fd4404a67a814ab106bd258bebb6facf955ade
Jorge Solano <jorgesolano92@gmail.com>
2023-05-30 14:49:05 -0400

    cambios generales

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // url: 'http://localhost:3001',
-  url: 'http://roadbook.guru:3001',
+  url: 'http://localhost:3001',
+  // url: 'http://roadbook.guru:3001',
 };
 
 /*
